49e8a00e6a790b217dd4b73ac0b530e66db529c8
Chris Li <licmike75@gmail.com>
2017-09-21 13:29:57 -0400

    Retrieved data from IBM api and made contact with the frontend

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const dotenv = require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const http = require("https");
+const cors = require('cors');
+const path = require('path');
 const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 const app = express();
 
 
 app.use(logger('dev'));
-
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -15,28 +18,40 @@ const watsonu = process.env.WATU;
 const watsonp = process.env.WATP;
 console.log('dotenv: ' + watsonp);
 
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19",
-    "method": "POST",
-    "headers": {
-      "authorization": "Basic ZmIzODBkMjgtYjQ0My00MGJiLTg1ZTYtY2FlYjgyNWM0ZmI5OjQ3dE5oR2xoalowVg==",
-      "cache-control": "no-cache",
-      "postman-token": "9d7c285d-0ad2-06bd-d307-e7118d02a20c"
-    },
-    "data": "{\n  \"text\": \"I am happy, very happy to see you again. So happy that I'm bleeding with happiness\"\n}"
-  }
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  });
+// request.get('https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19', {
+//     'auth': {
+//       'user': watsonu,
+//       'pass': watsonp,
+//       'sendImmediately': false
+//     },
+    
+//   })
+var request = require('request');
 
+let text= 'I am so happy that things work so well why is this happen in this way, Iam so hungry'
+var options = {
+    url: `https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19&text=${text}`,
+    auth: {
+        'user': watsonu,
+        'pass': watsonp
+    }
+};
 
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+        
+    }
+}
+
+request(options, callback);
 
 
 app.get('/api/hello', function(req,res) {
     res.json({message:'Hello World'});
+})
+app.get('/api/test', function(req,res) {
+    res.json({score: body});
 })
 
 app.get('*', function(req, res) {
