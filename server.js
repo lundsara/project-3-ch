ef8fe8a84c77fd1ba78dd6156b05fd6e0ed015fe
sarah lund <sarah.christine.lund@gmail.com>
2017-09-22 09:25:42 -0400

    added things

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-l
 const app = express();
 
 
+
 app.use(logger('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+const ebritek = process.env.EB;
 const watsonu = process.env.WATU;
 const watsonp = process.env.WATP;
 console.log('dotenv: ' + watsonp);
@@ -23,6 +25,14 @@ app.get('/api/hello', function(req,res) {
 
 app.get('*', function(req, res) {
     res.status(404).send({message: 'Oops! Not found'});
+});
+
+
+var request = require('request');
+app.get('/', function(req, res) {
+    request('https://www.eventbriteapi.com/v3/events/?token=${EB}', function(error, response, body) {
+        res.json(body)
+    });
 });
 
 // Setting up port & listen
