e1f3d0f31dc57e20688ac84885e40fd3a8ffd637
sarah lund <sarah.christine.lund@gmail.com>
2017-09-26 10:16:57 -0400

    application functioning

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Review = props => (
   <li id="box" key={props.review.id}>
     <h3>{props.review.title}</h3>
     <div>
-      <p>Posted by: {props.review.user}</p>
+      <p className="person">Posted by: {props.review.user}</p>
       {/* If i've been composed with a child, render */}
       {props.children}
       {/* {props.review.user === this.props.user.displayName || props.review.user === this.state.user.email ? */}
